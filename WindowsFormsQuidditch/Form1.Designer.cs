e58b68de5281e8f9c9bf3853082dc9085901b8f3
nicobascon <nico.bascon@live.fr>
2015-06-25 20:19:09 +0200

    Add windows form application using WS WCF

---
+++ WindowsFormsQuidditch/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsQuidditch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMatch = new System.Windows.Forms.Button();
+            this.listBoxMatch = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonMatch
+            // 
+            this.buttonMatch.Location = new System.Drawing.Point(180, 38);
+            this.buttonMatch.Name = "buttonMatch";
+            this.buttonMatch.Size = new System.Drawing.Size(146, 23);
+            this.buttonMatch.TabIndex = 0;
+            this.buttonMatch.Text = "Display matchs";
+            this.buttonMatch.UseVisualStyleBackColor = true;
+            this.buttonMatch.Click += new System.EventHandler(this.buttonMatch_Click);
+            // 
+            // listBoxMatch
+            // 
+            this.listBoxMatch.FormattingEnabled = true;
+            this.listBoxMatch.Location = new System.Drawing.Point(28, 86);
+            this.listBoxMatch.Name = "listBoxMatch";
+            this.listBoxMatch.Size = new System.Drawing.Size(463, 290);
+            this.listBoxMatch.TabIndex = 1;
+            this.listBoxMatch.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 450);
+            this.Controls.Add(this.listBoxMatch);
+            this.Controls.Add(this.buttonMatch);
+            this.Name = "Form1";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMatch;
+        private System.Windows.Forms.ListBox listBoxMatch;
+    }
+}
+
---
+++ WindowsFormsQuidditch/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WcfServiceLibrary;
+
+namespace WindowsFormsQuidditch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMatch_Click(object sender, EventArgs e)
+        {
+            this.listBoxMatch.Visible = true;
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            ICollection<CompositeType> matchs = (ICollection<CompositeType>) client.GetMatchs();
+            foreach (CompositeType match in matchs)
+            {
+                listBoxMatch.Items.Add(match.ToString());
+            }
+        }
+    }
+}
